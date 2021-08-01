512d2e92ba55fbe3fae7dcfb9f09e96a5c106fd9
Dave Artus <djna01@gmail.com>
2021-08-01 10:47:44 +0100

    Minimal model and controller

--- estate/src/components/Estate.vue
+++ estate/src/components/Estate.vue
@@ -44,131 +44,31 @@
 import { Client } from "paho-mqtt";
 let myClient;
 
-// experiments for graph colours
-const gradients = [
-  ["#222"],
-  ["#42b3f4"],
-  ["red", "orange", "yellow"],
-  ["purple", "violet"],
-  ["#00c6ff", "#F0F", "#FF0"],
-  ["#f72047", "#ffd200", "#1feaea"],
-];
-
 export default {
   name: "Estate",
 
   methods: {
-    makeThermostatDataTopic(groupId, propertyId) {
-      return "estate/thermostats/" + groupId + "/" + propertyId;
-    },
-    selectItem(item) {
-      console.log("Selected ", item);
-      const groupId = item[0].groupId;
-      if (!groupId) {
-        // not selecting a property
-        return;
-      }
-      const propertyId = item[0].propertyId;
-      if (
-        this.thermostatSeries.groupId >= 0 &&
-        this.thermostatSeries.propertyId >= 0
-      ) {
-        myClient.unsubscribe(
-          this.makeThermostatDataTopic(
-            this.thermostatSeries.groupId,
-            this.thermostatSeries.propertyId
-          ),
-          e => console.log("Unsubscribe error: ", e)
-        );
-        // new selection, clear graph
-        this.thermostatSeries.values.length = 0;
-        this.thermostatSeries.labels.length = 0;
-      }
-      myClient.subscribe(this.makeThermostatDataTopic(groupId, propertyId));
-      this.thermostatSeries.groupId = groupId;
-      this.thermostatSeries.groupName = item[0].groupName;
-      this.thermostatSeries.propertyId = propertyId;
-      this.thermostatSeries.propertyName = item[0].name;
-
-      return true;
-    },
-    // each thermostat resports its status
-    // update the model and the UX will display an indicator
-    processThermostatStatus(message) {
+    // Methods to process each different message type
+    processXxx(message) {
       const payload = JSON.parse(message.payloadString);
-      console.log(payload);
-      const group = this.propertyGroups.find((e) => {
-        return e.groupId === payload.groupId;
-      });
-      if (!group) {
-        console.log("unexpected group " + payload.groupId);
-        return;
-      }
-      const propertyIndex = group.children.findIndex((e) => {
-        return e.propertyId === payload.id;
-      });
-      if (propertyIndex >= 0) {
-        // easiest with Vue reactive arrays: replace entire item
-        const property = Object.assign({}, group.children[propertyIndex]);
-        property.online = payload.online;
-        // Vue helper for replacing array item
-        this.$set(group.children, propertyIndex, property);
-        console.log("groups after property update", this.propertyGroups);
-      }
-    },
-    // subscribed to one thermostat at a time, add new values to display 
-    processThermostatData(message) {
-      const payload = JSON.parse(message.payloadString);
-      const formattedTime = new Date(payload.time).toLocaleString([], {
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric"
-      });
-     
-      // record latest value for UX o dispay
-      this.thermostatSeries.latest = payload.temperature
+      console.log("Xxx payload", payload);
+      this.estateName = "Name from payload";
+      // extract data from payload and add into model
 
-      // add the latest value and time to the arrays
-      this.thermostatSeries.values.push(payload.temperature);
-      this.thermostatSeries.labels.push(formattedTime);
+      // Note: when updating data in an item in an array, replace
+      // the whole item using the Vue $set utility function
+      // which will trigger reactive behavious
+      // this.$set(someArray, anIndex, newItem);
 
-      // truncate so graph doesn't grow indefinately
-      const newLength = Math.min(this.thermostatSeries.values.length, 8);
-      this.thermostatSeries.values.length = newLength;
-      this.thermostatSeries.labels.length = newLength;
-    },
-    // HomeMonitor provides an estate hierarchy
-    // Perform some minor transformations to make data eaier to display
-    // in the tree and then later be able to identify an item that the user has selected:
-    // 1). Copy group id and name into each of its child properties
-    // 2). Give every item in the tree a unique id, otherwise wrong item is passed
-    // to selection callback.
-    processEstateOverview(message) {
-      const payload = JSON.parse(message.payloadString);
-      
-      // generate unique property ids, starting after highest group id
-      let uniqueId =  payload.propertyGroups.length ; 
-
-      payload.propertyGroups.forEach((group, gindex) => {
-        group.groupId = group.id; // the id by which the server knows this group
-        group.id = gindex; 
-
-        group.children.forEach((property) => {
-          property.propertyId = property.id; // the id by which the server knows this property
-          property.id = uniqueId++;
-          property.groupId = group.groupId;
-          property.groupName = group.name;
-        });
-        
-      });
-      this.propertyGroups = payload.propertyGroups;
-    },
+    }
+   
   },
 
   mounted() {
     // save "this" for use in Websocket callbacks
     const thisEstate = this;
 
+    // connect over websockets (need to make a unique client id if using more than one browser)
     myClient = new Client("localhost", 61614, "estateMonitor");
     myClient.onConnectionLost = onConnectionLost;
     myClient.onMessageArrived = onMessageArrived;
@@ -177,8 +77,9 @@ export default {
     function onConnect() {
       // Once a connection has been made, make a subscription and send a message.
       console.log("onConnect");
-      myClient.subscribe("estate/Overview");
-      myClient.subscribe("estate/online/thermostats");
+      myClient.subscribe("some/topic");
+      myClient.subscribe("some/different/topic");
+     
     }
     function onConnectionLost(responseObject) {
       if (responseObject.errorCode !== 0)
@@ -187,51 +88,33 @@ export default {
 
     function onMessageArrived(message) {
       console.log("onMessageArrived:" + message.topic + ":");
-      if (message.topic === "estate/Overview") {
-        thisEstate.processEstateOverview(message);
-      } else if (message.topic === "estate/online/thermostats") {
-        thisEstate.processThermostatStatus(message);
+      if (message.topic === "some/topic") {
+        thisEstate.processXyy(message);
+      } else if (message.topic === "some/different/topic") {
+        // adjust to call suitable method
+        // thisEstate.processYyy(message);
       } else {
-        thisEstate.processThermostatData(message);
+        // adjust to call suitable method
+        // thisEstate.processZzz(message);
       }
     }
   },
 
   data: () => ({
-    // diplay in tree, start with fake item
-    propertyGroups: [
+    // predefine the model, so that Vue makes it reactive
+    // For each subscribed message define a structure for the
+    // data you will refer to in the UX; set up the structure
+    // to suit the Vue components you will use and where 
+    // needed transform the data received in the message to
+    // fit this structure.
+    estateName : "unknown",
+    someEstateData: [
       {
         id: 0,
         name: "Loading ...",
       },
     ],
-    activeItems : [],
-    // display in graphe
-    thermostatSeries: {
-      groupId: -1,
-      groupName : "",
-      propertyId: -1,
-      propertyName: "",
-      latest: "",
-      labels: [],
-      values: [],
-    },
-    // presentation style for graph
-    thermostatGraphDef: {
-      width: 2,
-      radius: 10,
-      padding: 8,
-      lineCap: "round",
-      gradient: gradients[5],
-      labels: [],
-      'label-size': 5,
-      value: [],
-      gradientDirection: "top",
-      gradients,
-      fill: false,
-      type: "trend",
-      autoLineWidth: false,
-    },
+  
   }),
 };
 </script>
