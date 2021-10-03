b4261c8f50309078d7806d4f5449ae1cfa26bd16
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2021-10-03 08:09:40 +0100

    add connecton error handling and message display

--- webapp/wsmqtt-eg.js
+++ webapp/wsmqtt-eg.js
@@ -1,18 +1,22 @@
-client = new Paho.MQTT.Client("localhost", 61614, "clientId");
+client = new Paho.MQTT.Client("localhost", 61614, "BrowserClientId");
 client.onConnectionLost = onConnectionLost;
 
 client.onMessageArrived = onMessageArrived;
-client.connect({onSuccess:onConnect});
+client.connect(
+    {onSuccess:onConnect, onFailure: onConnectionFailed}
+);
 
 function onConnect() {
   // Once a connection has been made, make a subscription and send a message.
   console.log("onConnect");
   client.subscribe("home/thermostats");
-  //message = new Paho.MQTT.Message("Hello");
-  //message.destinationName = "/World";
-  //client.send(message); 
 };
 
+
+function onConnectionFailed(responseObject) {
+  if (responseObject.errorCode !== 0)
+	console.log("onConnectionFailed:"+responseObject.errorMessage);
+};
 
 function onConnectionLost(responseObject) {
   if (responseObject.errorCode !== 0)
@@ -20,5 +24,9 @@ function onConnectionLost(responseObject) {
 };
 function onMessageArrived(message) {
   console.log("onMessageArrived:"+message.payloadString);
-  // client.disconnect(); 
+  let telemetry = JSON.parse(message.payloadString);
+   console.log("Telemetry: " + telemetry.location 
+               + ", " + telemetry.temperature
+               + ", " + new Date(telemetry.date)
+              );
 };
