08db8320b8b1ca51b41698eb2f7e3a4f0125455d
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2022-05-08 08:29:02 +0100

    selector by priority

--- receive.py
+++ receive.py
@@ -25,9 +25,15 @@ queueName = "roomBooking"
 if len(sys.argv) > 1 and len(sys.argv[1]) > 0 :
     queueName = sys.argv[1]
 
+headers = {}
+prioritySelector = ""
+if len(sys.argv) > 2 and len(sys.argv[2]) > 0 :
+    prioritySelector = sys.argv[2]
+    headers['selector'] = prioritySelector
+
 queueId = "/queue/" + queueName
 
-conn.subscribe(destination=queueId, id=1, ack='auto')
+conn.subscribe(destination=queueId, id=1, ack='auto', headers=headers)
 print('subscribed to ' + queueId)
 time.sleep(200)
 conn.disconnect()
