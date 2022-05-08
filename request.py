c951169efc017ca2d7a316c0be0f6511df3b4e25
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2022-05-08 08:09:08 +0100

    set headers

--- request.py
+++ request.py
@@ -28,9 +28,23 @@ queueName = "roomBooking"
 if len(sys.argv) > 2 and len(sys.argv[2]) > 0 :
     queueName = sys.argv[2]
 
+priorityString = "1"
+if len(sys.argv) > 3 and len(sys.argv[3]) > 0 :
+    priorityString = sys.argv[3]
+
+priority = 1
+try:
+    priority = int(priorityString)
+except ValueError:
+    print(f"{priorityString} is not a valid integer, using default {priority}.")
+    
 queueId = "/queue/" + queueName
 
-conn.send(body=message, destination=queueId)
+headers = {}
+headers['priority'] = priority
+headers['djna'] = "wibble"
+
+conn.send(body=message, destination=queueId, headers=headers)
 conn.disconnect()
 print('sent :' + message + ": to " + queueId)
 
